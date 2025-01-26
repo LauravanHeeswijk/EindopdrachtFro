fc87d573b228304a6c3276c8eb00d0ab03937a57
LauravanHeeswijk <lvanheeswijk@live.nl>
2025-01-26 23:55:24 +0100

    Added folder structure and updated project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+    return (
+        <div>
+            <h1>Mijn nieuwe React App</h1>
+            <p>Welkom bij mijn project!</p>
+        </div>
+    );
+}
+
+export default App;
+
